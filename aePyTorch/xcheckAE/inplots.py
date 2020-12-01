c80eb611cb4dca880c50fcdb44b7b3852dd05378
Vasilis Belis <billbelis@yahoo.com>
2020-12-01 16:56:23 +0100

    plots of input variables

--- aePyTorch/xcheckAE/inplots.py
+++ aePyTorch/xcheckAE/inplots.py
@@ -1,20 +1,16 @@
 import numpy as np
-<<<<<<< HEAD
 import matplotlib.pyplot as plt
 #Unormalised vars:
 bkg = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/raw_bkg.npy')
 sig = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/raw_sig.npy')
-
+outdir = 'pdfsIn'
+if not(os.path.exists(outdir)):
+	os.mkdir(outdir)
 for i in range(bkg.shape[1]):
     hSig,_,_ = plt.hist(x=sig[:,i],density=1,bins=60,alpha=0.6,histtype='step',linewidth=2.5,label='Sig')
     hBkg,_,_ = plt.hist(x=bkg[:,i],density=1,bins=60,alpha=0.6,histtype='step',linewidth=2.5,label='Bkg')
     plt.xlabel(f'feature {i}')
     plt.ylabel('Entries/Bin')
     plt.legend()
-    plt.savefig(f'inputPlots/plot{i}.png')
+    plt.savefig(outdir+f'/plot{i}.png')
     plt.clf()
-
-=======
-
-sig = np.load(
->>>>>>> 90b709607e4840aeaa3e71938811950e924090f3

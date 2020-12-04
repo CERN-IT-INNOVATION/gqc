New correct normalization eliminating the sig -vs- bkg input bias

aePyTorch/testPlots.py
@@ -13,7 +13,7 @@ from varname import *
 def ratioPlotter(inp,out,ifeature,classLabel = ''):
 	hIn,binsIn, patchIn = plt.hist(x=inp,bins=60,range =(0,1),alpha=0.5,histtype='step',linewidth=2.5,label='Original '+classLabel)
 	hOut,binsOut, patch = plt.hist(x=out,bins=60,range = (0,1),alpha=0.5,histtype='step',linewidth = 2.5,label='Output '+classLabel)
-	plt.xlabel('feature '+str(ifeature))
+	plt.xlabel('feature '+varname(i))
 	plt.ylabel('Entries/Bin')
 	plt.title('Distribution of '+varname(i))
 	plt.legend()
@@ -96,6 +96,8 @@ with torch.no_grad():
 		plt.clf()
 	
 	samples = ['Sig','Bkg']
+	testLoaderSig = torch.utils.data.DataLoader(arrayData(testDataSig),batch_size = batch_size,shuffle = False)
+	testLoaderBkg = torch.utils.data.DataLoader(arrayData(testDataBkg),batch_size = batch_size,shuffle = False)
 	loaders = [testLoaderSig,testLoaderBkg]
 	colors = ['b','r']
 	labels = ['Test on Sig.', 'Test on Bkg.']
@@ -113,4 +115,5 @@ with torch.no_grad():
 		plt.xlabel('MSE per Batch')
 		plt.title('MSE per batch, Ntest={}.'.format(len(testDataset)))
 	plt.legend()
-	plt.savefig(savedModel+'/testlossHist.png')
+
+

normalise_features.py
@@ -9,34 +9,27 @@ import numpy as np
         lep_feats = ["pt","eta","phi","en","px","py","pz"],
         evdesc_feats = ["nleps", "njets", "nbtags", "nMatch_wq", "nMatch_tb", "nMatch_hb"],#Don't fit in AE!!
 '''
-def normalizer(infile,outfile):
-	dataset = np.load(infile)
-	dataset_normed = np.zeros_like(dataset.T)
+#Simultaneous trsf for sig and bkg:
+#Using numpy broadcasting without for loops for efficiency (loops run in C and not in Python)
+def normalizer(bkgfile,sigfile,outfile):
+	bkg,sig = np.load(bkgfile), np.load(sigfile)
+	maxBkg,maxSig = np.amax(bkg,axis=0),np.amax(sig,axis=0)
+	minBkg,minSig = np.amin(sig,axis=0),np.amin(bkg,axis=0)
 	
-	for i,ifeature in enumerate(dataset.T):#to iterate over the columns i.e. features and not events
-		xmax, xmin = np.amax(ifeature), np.amin(ifeature)
-		if xmax != xmin:#not cause NaN's
-			ifeature_normed = (ifeature-xmin)/(xmax-xmin)
-			dataset_normed[i] = ifeature_normed
-		else:
-			dataset_normed[i] = ifeature
-	#for i,ifeature in enumerate(dataset_normed):
-	#	print(ifeature)
-	np.save(outfile,dataset_normed.T)
-	return None
+	#Find global max/min for each feature for sig AND bkg
+	#-->Correct normalization to retain the shapes of the pdfs
+	maxTot = np.amax(np.vstack((maxBkg,maxSig)),axis=0)
+	minTot = np.amin(np.vstack((minBkg,minSig)),axis=0)
+	
+	bkgNorm = (bkg-minTot)/(maxTot-minTot)
+	sigNorm = (sig-minTot)/(maxTot-minTot)
+	np.save(outfile+'Bkg.npy',bkgNorm)
+	np.save(outfile+'Sig.npy',sigNorm)
 
-#Training
-print('Training + Testing Samples')
-infile = "input_ae/raw_sig.npy"
-outfile = "input_ae/trainingTestingDataSig.npy"
-print('Normalizing: '+infile)
-normalizer(infile,outfile)
+infileBkg,infileSig = 'input_ae/raw_bkg.npy','input_ae/raw_sig.npy'
+print('Normalizing: '+infileBkg+' & '+infileSig)
+outfile = "input_ae/trainingTestingData"
+normalizer(infileBkg,infileSig,outfile)
 print('Output: ',outfile)
 
-infile = "input_ae/raw_bkg.npy"
-outfile = "input_ae/trainingTestingDataBkg.npy"
-print('Normalizing: '+infile)
-normalizer(infile,outfile)
-print('Output: ',outfile)
 
-#
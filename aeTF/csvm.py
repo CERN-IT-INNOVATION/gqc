437786981c7ced61225c6bcf2f1279136b38bff4
Samuel Gonzalez Castillo <samuel@sgc.ink>
2020-12-08 14:40:27 +0100

    Add classical svm for TF

--- aeTF/csvm.py
+++ aeTF/csvm.py
@@ -4,23 +4,23 @@ from tensorflow.keras.models import load_model
 
 from sklearn import svm
 
-model = "best"
+model = "D-7-30-16-F2"
 
 ae = load_model("out/" + model + "/model")
-#X_tr = np.array(ae.encoder(vali_bkg));
-#Y_tr = np.array(ae.encoder(vali_sig));
-X_tr = np.array(vali_bkg);
-Y_tr = np.array(vali_sig);
+X_tr = np.array(ae.encoder(vali_bkg));
+Y_tr = np.array(ae.encoder(vali_sig));
+#X_tr = np.array(vali_bkg);
+#Y_tr = np.array(vali_sig);
 train = np.vstack((X_tr, Y_tr));
 labels = ['x'] * X_tr.shape[0] + ['y'] * Y_tr.shape[0]
 
-#X = np.array(ae.encoder(test_bkg)).tolist();
-#Y = np.array(ae.encoder(test_sig)).tolist();
-X = np.array(test_bkg).tolist();
-Y = np.array(test_sig).tolist();
+X = np.array(ae.encoder(test_bkg)).tolist();
+Y = np.array(ae.encoder(test_sig)).tolist();
+#X = np.array(test_bkg).tolist();
+#Y = np.array(test_sig).tolist();
 
 
-
+print("Hello")
 cls = svm.SVC()
 cls.fit(train, labels);
 
@@ -28,4 +28,5 @@ resX = cls.predict(X)
 resY = cls.predict(Y)
 print("Success ratio bkg: ", sum(resX == 'x') / (sum(resX == 'x') + sum(resX == 'y')))
 print("Success ratio sig: ", sum(resY == 'y') / (sum(resY == 'x') + sum(resY == 'y')))
+print("Success ratio: ", (sum(resX == 'x') + sum(resY == 'y')) / (len(resX) + len(resY)))
 

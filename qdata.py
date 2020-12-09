02f0011b6f49aa85aa1dc5bc284e6b30b8114cbd
Samuel Gonzalez Castillo <samuel@sgc.ink>
2020-12-09 10:52:14 +0100

    Add data labels to qdata

--- qdata.py
+++ qdata.py
@@ -14,8 +14,12 @@ ntrain, nvalid, ntest = int(ntot*0.8), int(0.1*ntot), int(0.1*ntot)
 print(f'Loaded data for Quantum classifier: ntrain = {ntrain}, nvalid = {nvalid}, ntest = {ntest} ')
 
 train = np.vstack((testSigAE[:ntrain],testBkgAE[:ntrain]))
+train_labels = ['s'] * ntrain + ['b'] * ntrain;
 validation = np.vstack((testSigAE[ntrain:(ntrain+nvalid)],testBkgAE[ntrain:(ntrain+nvalid)]))
+validation_labels = ['s'] * nvalid + ['b'] * nvalid;
 test = np.vstack((testSigAE[(ntrain+nvalid):],testBkgAE[(ntrain+nvalid):]))
+test_labels = ['s'] * ntest + ['b'] * ntest;
+
 
 if np.array_equal(validation,test):
 	raise Exception('Validation and Testing datasets are the same!')

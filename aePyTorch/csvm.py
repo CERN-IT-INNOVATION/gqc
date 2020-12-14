b09eacc0f3a3707fd4fe3120b5e9a894af5cbd58
Vasilis Belis <billbelis@yahoo.com>
2020-12-14 13:03:41 +0100

    Clean code and new csvm test on SigmoidLatent

--- aePyTorch/csvm.py
+++ aePyTorch/csvm.py
@@ -1,15 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import os
 import warnings
 import torch
-import torch.nn as nn
 from model import * #Load custom model
 from splitDatasets import *
 import argparse
+from sklearn import svm
 
 
-#use gpu if available
+#########################TODO use encode module to load the latent data:
 with warnings.catch_warnings():
 	warnings.simplefilter("ignore")
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -17,9 +15,19 @@ with warnings.catch_warnings():
 infiles = ('../input_ae/trainingTestingDataSig.npy','../input_ae/trainingTestingDataBkg.npy')
 
 savedModel = "trained_models/L64.52.44.32.24.16B128Lr3e-03MoreDataNewNorm/"
-layers = [64, 52, 44, 32, 24, 16]
+defaultlayers = [64, 52, 44, 32, 24, 16]
 
-_,validDataset,testDataset,validLabels, testLabels = splitDatasets(infiles, labels=True)#Load test samples
+parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)#include defaults in -h
+parser.add_argument("--input", type=str, default=infiles, nargs =2, help="path to datasets")
+parser.add_argument('--model',type=str,required=True,default=savedModel,help='path to saved model')
+parser.add_argument('--layers',type=int,default=defaultlayers,nargs='+',help='type hidden layers nodes corresponding to saved model')
+
+args = parser.parse_args()
+infiles = args.input
+(savedModel,layers) = (args.model,args.layers)
+
+_,validDataset,testDataset,_,validLabels, testLabels = splitDatasets(infiles, labels=True)#Load test samples
 testLoader = torch.utils.data.DataLoader(arrayData(testDataset),batch_size = testDataset.shape[0],shuffle = False)
 validLoader = torch.utils.data.DataLoader(arrayData(validDataset),batch_size = validDataset.shape[0],shuffle = False)
 
@@ -30,27 +38,21 @@ model = AE(node_number = layers).to(device)
 model.load_state_dict(torch.load(savedModel+'bestModel.pt'))
 model.eval()
 
-criterion = nn.MSELoss(reduction= 'mean')
-
-#if __name__ == "__main__":
 testDataSig, testDataBkg = splitDatasets(infiles,separate=True)
-testLoaderSig = torch.utils.data.DataLoader(arrayData(testDataSig),batch_size = testDataSig.shape[0],shuffle = False)
-testLoaderBkg = torch.utils.data.DataLoader(arrayData(testDataBkg),batch_size = testDataBkg.shape[0],shuffle = False)
 
-#Latent pdf's & input-vs-output pdf's:
+#Model output for the SVM training input:
 with torch.no_grad():
-	#MSE on whole validation & test samples:
 	dataIter = iter(testLoader)
 	inp = dataIter.next()
 	_, testOutput = model(inp.float())
 	dataIter = iter(validLoader)
 	inp = dataIter.next()
 	_, validOutput = model(inp.float())
+################################################################################################
 
 
-from sklearn import svm
-
-
+#TODO save svm results in SVM log automatically
+#TODO create accessible Ntrain variable, and benchmark qsvm with csvm give the same training size
 train = np.array(validOutput)
 labels = np.array(validLabels)
 

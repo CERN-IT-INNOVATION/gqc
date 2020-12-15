a2f80c30ef10d132f541104d8e97a027b63501b8
Vasilis Belis <billbelis@yahoo.com>
2020-12-15 16:35:50 +0100

    CSVM with encode module

--- aePyTorch/csvm.py
+++ aePyTorch/csvm.py
@@ -3,61 +3,46 @@ import warnings
 import torch
 from model import * #Load custom model
 from splitDatasets import *
-import argparse
+import argparse, time
 from sklearn import svm
+from encode import *
 
-
-#########################TODO use encode module to load the latent data:
+start_time = time.time()
 with warnings.catch_warnings():
 	warnings.simplefilter("ignore")
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 infiles = ('../input_ae/trainingTestingDataSig.npy','../input_ae/trainingTestingDataBkg.npy')
-
 savedModel = "trained_models/L64.52.44.32.24.16B128Lr3e-03MoreDataNewNorm/"
 defaultlayers = [64, 52, 44, 32, 24, 16]
 
 parser = argparse.ArgumentParser()
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)#include defaults in -h
 parser.add_argument("--input", type=str, default=infiles, nargs =2, help="path to datasets")
-parser.add_argument('--model',type=str,required=True,default=savedModel,help='path to saved model')
+parser.add_argument('--model',type=str,default=savedModel,help='path to saved model')
 parser.add_argument('--layers',type=int,default=defaultlayers,nargs='+',help='type hidden layers nodes corresponding to saved model')
 
 args = parser.parse_args()
 infiles = args.input
 (savedModel,layers) = (args.model,args.layers)
 
-_,validDataset,testDataset,_,validLabels, testLabels = splitDatasets(infiles, labels=True)#Load test samples
-testLoader = torch.utils.data.DataLoader(arrayData(testDataset),batch_size = testDataset.shape[0],shuffle = False)
-validLoader = torch.utils.data.DataLoader(arrayData(validDataset),batch_size = validDataset.shape[0],shuffle = False)
 
+_,validDataset,testDataset,_,validLabels, testLabels = splitDatasets(infiles, labels=True)
 feature_size = testDataset.shape[1]
 layers.insert(0,feature_size)
 
-model = AE(node_number = layers).to(device)
-model.load_state_dict(torch.load(savedModel+'bestModel.pt'))
-model.eval()
-
-testDataSig, testDataBkg = splitDatasets(infiles,separate=True)
-
-#Model output for the SVM training input:
-with torch.no_grad():
-	dataIter = iter(testLoader)
-	inp = dataIter.next()
-	_, testOutput = model(inp.float())
-	dataIter = iter(validLoader)
-	inp = dataIter.next()
-	_, validOutput = model(inp.float())
-################################################################################################
-
-
 #TODO save svm results in SVM log automatically
-#TODO create accessible Ntrain variable, and benchmark qsvm with csvm give the same training size
-train = np.array(validOutput)
+#TODO Use also qdata to have fair benchmark with qclassifiers
+#TODO Have accessible Ntrain variable
+
+train = encode(validDataset,savedModel,layers)
 labels = np.array(validLabels)
 
-test = np.array(testOutput)
+test = encode(testDataset,savedModel,layers)
 tlabels = np.array(testLabels)
+
+print(f'Training samples for SVM: {len(train)}.')
+print(f'Testing samples for SVM: {len(test)}.')
 
 cls = svm.SVC()
 cls.fit(train, labels);
@@ -65,4 +50,7 @@ cls.fit(train, labels);
 res = np.array(cls.predict(test))
 
 print("Success ratio: ", sum(res == tlabels) / len(res))
+
+end_time = time.time()
+print(f"Execution Time {end_time-start_time}")
 
--- aePyTorch/encode.py
+++ aePyTorch/encode.py
@@ -1,22 +1,14 @@
 import numpy as np
 import warnings
 import torch
-import torch.nn as nn
-from aePyTorch.model import * #Load custom model
+from model import * #Load custom model
 
-
-#use gpu if available
 with warnings.catch_warnings():
 	warnings.simplefilter("ignore")
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-savedModel = "aePyTorch/trained_models/L64.52.44.32.24.16B128Lr3e-03MoreDataNewNorm/"
-layers = [64, 52, 44, 32, 24, 16]
-feature_size = 67
-layers.insert(0,feature_size)
-
-def encode_array(my_data):
-	dataLoader = torch.utils.data.DataLoader(arrayData(my_data),batch_size = my_data.shape[0],shuffle = False)
+def encode_array(data,savedModel,layers):
+	dataLoader = torch.utils.data.DataLoader(arrayData(data),batch_size = data.shape[0],shuffle = False)
 
 	model = AE(node_number = layers).to(device)
 	model.load_state_dict(torch.load(savedModel+'bestModel.pt'))
@@ -28,10 +20,10 @@ def encode_array(my_data):
 		_, output = model(inp.float())
 		return np.array(output);
 
-def encode(my_data):
-	if (isinstance(my_data, dict)):
-		for x in my_data:
-			my_data[x] = encode_array(my_data[x])
-		return my_data;
+def encode(data,savedModel,layers):
+	if (isinstance(data, dict)):
+		for x in data:
+			data[x] = encode_array(data[x],savedModel,layers)
+		return data;
 	
-	return encode_array(my_data);	
+	return encode_array(data,savedModel,layers);	

6709df734eebf2d1b67748d094688c4727c0a275
Vasilis Belis <billbelis@yahoo.com>
2020-12-16 09:50:50 +0100

    Add execution timer

--- vqc.py
+++ vqc.py
@@ -3,6 +3,7 @@ from qiskit.aqua.components.optimizers import CRS, TNC, ISRES # Classical optimi
 from qiskit.circuit.library.n_local.two_local import TwoLocal # Generates the variational circuit.
 from qiskit.aqua.components.feature_maps.raw_feature_vector import RawFeatureVector # Amplitude encoding.
 
+import time
 from qiskit import Aer
 from qiskit.aqua import QuantumInstance
 from qiskit.aqua.algorithms import VQC
@@ -10,6 +11,7 @@ from qiskit.aqua.algorithms import VQC
 import qdata
 from aePyTorch.encode import encode
 
+start_time = time.time()
 feature_dim = 4
 var_form = TwoLocal(feature_dim, 'ry', 'cx', 'linear', reps = 3, insert_barriers=False)
 feature_map = RawFeatureVector(2**feature_dim)
@@ -25,3 +27,5 @@ quantum_instance = QuantumInstance(backend, shots=1024, seed_simulator=seed, see
 result = vqc.run(quantum_instance)
 
 print("testing success ratio: {}".format(result['testing_accuracy']))
+end_time = time.time()
+print(f"Execution Time {end_time-start_time} s")

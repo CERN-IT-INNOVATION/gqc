bdba134e4299d04cb764ca6ade04eb0c72bf3bfd
Vasilis Belis <billbelis@yahoo.com>
2021-01-19 18:16:57 +0100

    add todo's

--- aePyTorch/xcheckAE/inplots.py
+++ aePyTorch/xcheckAE/inplots.py
@@ -9,10 +9,16 @@ from varname import *
 #bkg = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/trainingTestingDataBkg.npy')
 #sig = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/trainingTestingDataSig.npy')
 #New norm:
-bkg = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/trainingTestingDataBkgNew.npy')
-sig = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/trainingTestingDataSigNew.npy')
+#bkg = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/trainingTestingDataBkgNew.npy')
+#sig = np.load('/data/vabelis/disk/sample_preprocessing/input_ae/trainingTestingDataSigNew.npy')
+#New mean sigma norm:
+#bkg = np.load('/data/vabelis/disk/qml/input_ae/trainingTestingDataBkgMeanSigma.npy')
+#sig = np.load('/data/vabelis/disk/qml/input_ae/trainingTestingDataSigMeanSigma.npy')
+#No px,py,pz:
+bkg = np.load('/data/vabelis/disk/qml/input_ae/trainingTestingDataBkgnoCart.npy')
+sig = np.load('/data/vabelis/disk/qml/input_ae/trainingTestingDataSignoCart.npy')
 
-outdir = 'pdfsInNormNew'
+outdir = 'pdfsInNormNoCart'
 if not(os.path.exists(outdir)):
 	os.mkdir(outdir)
 for i in range(bkg.shape[1]):
@@ -22,7 +28,11 @@ for i in range(bkg.shape[1]):
 	xmin = min(np.amin(pdfSig),np.amin(pdfBkg))
 	ax.hist(x=pdfSig,density=1,bins=60,range=(xmin,xmax),alpha=0.6,histtype='step',linewidth=2.5,label='Sig')
 	ax.hist(x=pdfBkg,density=1,bins=60,range=(xmin,xmax),alpha=0.6,color ='r',histtype='step',linewidth=2.5,label='Bkg')
-	plt.xlabel('feature '+varname(i))
+	#ax.hist(x=pdfSig,density=1,bins=60,alpha=0.6,histtype='step',linewidth=2.5,label='Sig')
+	#ax.hist(x=pdfBkg,density=1,bins=60,alpha=0.6,color ='r',histtype='step',linewidth=2.5,label='Bkg')
+	#plt.xlabel('feature '+varname(i))
+	#Need to remake varname without px,py,pz:
+	plt.xlabel('feature '+str(i))
 	plt.ylabel('Entries/Bin')
 	plt.legend()
 
@@ -31,5 +41,7 @@ for i in range(bkg.shape[1]):
 	#box = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 	#textstr = '\n'.join((f'Sig. mean={np.mean(pdfSig):.2f}',f'Sig. std={np.std(pdfSig):.2f}',f'Bkg. mean={np.mean(pdfBkg):.2f}',f'Bkg. std={np.std(pdfBkg):.2f}' ))	
 	#ax.text(0.95, 0.95, textstr, transform=ax.transAxes, fontsize=14,verticalalignment='top', bbox=box)
-	plt.savefig(outdir+'/plot'+varname(i)+'.png')
+	#plt.savefig(outdir+'/plot'+varname(i)+'.png')
+	#Need to remake varname without px,py,pz:
+	plt.savefig(outdir+'/plot'+str(i)+'.png')
 	plt.close(fig)
--- qdata.py
+++ qdata.py
@@ -2,7 +2,11 @@ import numpy as np
 from aePyTorch.splitDatasets import splitDatasets
 
 #Load the test dataset of the autoencoder and split it to training,validation and testing datasets for the qml
-infiles = ('input_ae/trainingTestingDataSig.npy','input_ae/trainingTestingDataBkg.npy')
+#TODO: If we use the dataset with more data?->make it a qdata class!
+
+#infiles = ('input_ae/trainingTestingDataSig.npy','input_ae/trainingTestingDataBkg.npy')
+
+infiles = ('input_ae/trainingTestingDataSig7.2e5.npy','input_ae/trainingTestingDataBkg7.2e5.npy')
 trainSigAE, trainBkgAE, validSigAE, validBkgAE, testSigAE,testBkgAE = splitDatasets(infiles,separate=True, not_all = False)
 
 ntot_test = int(testSigAE.shape[0])
@@ -12,7 +16,7 @@ if ntot_test != testBkgAE.shape[0] or ntot_train != trainSigAE.shape[0] or ntot_
 	raise Exception('nSig != nBkg!!! Events should be equal')
 
 # TODO: Define global var to define the ntrain etc samples from the script importing this module.
-ntrain, nvalid, ntest = int(ntot_train*0.001), int(0.004*ntot_valid), int(0.004*ntot_test)
+ntrain, nvalid, ntest = int(ntot_train*0.0005), int(0.002*ntot_valid), int(0.002*ntot_test)
 print(f'Loaded data for Quantum classifier: ntrain = {ntrain}, nvalid = {nvalid}, ntest = {ntest} ')
 
 train = np.vstack((trainSigAE[:ntrain],trainBkgAE[:ntrain]))

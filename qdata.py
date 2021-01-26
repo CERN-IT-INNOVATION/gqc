a67988fef7e168e69d40dbea7830cd04bf65b52d
Samuel Gonzalez Castillo <samuel@sgc.ink>
2021-01-26 15:15:12 +0100

    Use a class structure for qdata

--- qdata.py
+++ qdata.py
@@ -1,34 +1,44 @@
 import numpy as np
 from aePyTorch.splitDatasets import splitDatasets
 
-#Load the test dataset of the autoencoder and split it to training,validation and testing datasets for the qml
-infiles = ('input_ae/trainingTestingDataSig.npy','input_ae/trainingTestingDataBkg.npy')
-trainSigAE, trainBkgAE, validSigAE, validBkgAE, testSigAE,testBkgAE = splitDatasets(infiles,separate=True, not_all = False)
+class qdata:
 
-ntot_test = int(testSigAE.shape[0])
-ntot_train = int(trainSigAE.shape[0])
-ntot_valid = int(validSigAE.shape[0])
-if ntot_test != testBkgAE.shape[0] or ntot_train != trainSigAE.shape[0] or ntot_valid != validSigAE.shape[0]:
-	raise Exception('nSig != nBkg!!! Events should be equal')
+	#Load the test dataset of the autoencoder and split it to training,validation and testing datasets for the qml
+	infiles = ('input_ae/trainingTestingDataSig.npy','input_ae/trainingTestingDataBkg.npy')
+	trainSigAE, trainBkgAE, validSigAE, validBkgAE, testSigAE,testBkgAE = splitDatasets(infiles,separate=True, not_all = False)
+	
+	ntot_test = int(testSigAE.shape[0])
+	ntot_train = int(trainSigAE.shape[0])
+	ntot_valid = int(validSigAE.shape[0])
 
-# TODO: Define global var to define the ntrain etc samples from the script importing this module.
-ntrain, nvalid, ntest = int(ntot_train*0.001), int(0.004*ntot_valid), int(0.004*ntot_test)
-print(f'Loaded data for Quantum classifier: ntrain = {ntrain}, nvalid = {nvalid}, ntest = {ntest} ')
 
-train = np.vstack((trainSigAE[:ntrain],trainBkgAE[:ntrain]))
-train_labels = np.array(['s'] * ntrain + ['b'] * ntrain)
-train_dict = {'s': trainSigAE[:ntrain], 'b': trainBkgAE[:ntrain]}
+	def __init__(self, train_p = 0.001, valid_p = 0.004, test_p = 0.004, proportion = True):
+		if proportion:
+			ntrain = int(self.ntot_train*train_p)
+			nvalid = int(self.ntot_valid*valid_p)
+			ntest = int(self.ntot_test*test_p)
+		else:
+			ntrain = train_p
+			nvalid = valid_p
+			ntest = test.p
 
-validation = np.vstack((validSigAE[:nvalid],validBkgAE[:nvalid]))
-validation_labels = np.array(['s'] * nvalid + ['b'] * nvalid)
-validation_dict = {'s': validSigAE[:nvalid], 'b': validBkgAE[:nvalid]}
+		self.ntrain = ntrain
+		self.nvalid = nvalid
+		self.ntest = ntest
 
-test = np.vstack((testSigAE[:ntest],testBkgAE[:ntest]))
-test_labels = np.array(['s'] * ntest + ['b'] * ntest)
-test_dict = {'s': testSigAE[:ntest], 'b': testBkgAE[:ntest]}
+		#print(f'Loaded data for Quantum classifier: ntrain = {ntrain}, nvalid = {nvalid}, ntest = {ntest} ')
 
-if np.array_equal(validation,test):
-	raise Exception('Validation and Testing datasets are the same!')
+		self.train = np.vstack((self.trainSigAE[:ntrain], self.trainBkgAE[:ntrain]))
+		self.train_labels = np.array(['s'] * ntrain + ['b'] * ntrain)
+		self.train_dict = {'s': self.trainSigAE[:ntrain], 'b': self.trainBkgAE[:ntrain]}
 
-print(f'xcheck: train/validation/test shapes: {train.shape}/{validation.shape}/{test.shape}')
+		self.validation = np.vstack((self.validSigAE[:nvalid],self.validBkgAE[:nvalid]))
+		self.validation_labels = np.array(['s'] * nvalid + ['b'] * nvalid)
+		self.validation_dict = {'s': self.validSigAE[:nvalid], 'b': self.validBkgAE[:nvalid]}
+
+		self.test = np.vstack((self.testSigAE[:ntest],self.testBkgAE[:ntest]))
+		self.test_labels = np.array(['s'] * ntest + ['b'] * ntest)
+		self.test_dict = {'s': self.testSigAE[:ntest], 'b': self.testBkgAE[:ntest]}
+
+		print(f'xcheck: train/validation/test shapes: {self.train.shape}/{self.validation.shape}/{self.test.shape}')
 

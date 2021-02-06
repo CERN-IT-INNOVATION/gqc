8851b3bdd96d1c48cef811cf765c1c4b7c544051
Samuel Gonzalez Castillo <samuel@sgc.ink>
2021-02-06 16:19:47 +0100

    Add Qiskit-VQC upates

--- aeTF/encode.py
+++ aeTF/encode.py
@@ -8,7 +8,7 @@ ae = load_model("aeTF/out/" + model + "/model")
 def encode(data):
 	if (isinstance(data, dict)):
 		for x in data:
-			data[x] = np.array(ae.encoder(data[x]))
+			data[x] = np.array(ae.encoder(np.copy(data[x])))
 		return data;
 	
-	return np.array(ae.encoder(data));
+	return np.array(ae.encoder(np.copy(data)));

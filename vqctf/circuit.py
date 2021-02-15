0a917df5a50cf7f6fc2518f01c00b5170bb53f78
Samuel Gonzalez Castillo <samuel@sgc.ink>
2021-02-15 22:38:26 +0100

    Switch to PL-TF for the VQC

---
+++ vqctf/circuit.py
@@ -0,0 +1,41 @@
+import pennylane as qml
+import numpy as np
+
+import vqctf.zzlane as fmap
+import vqctf.twolane as vform
+
+nqubits = 4
+
+circuit_desc = "4-qubits\\ZZ(4) 2L(2rep) ZZ(4) 2L(2rep)"
+
+dev = qml.device("default.qubit", wires=nqubits)
+
+state_0 = [[1], [0]]
+state_1 = [[0], [1]]
+states = [state_0, state_1]
+
+def density_matrix(state):
+	return state * np.conj(state).T
+
+y = density_matrix(state_0)
+
+@qml.qnode(dev, interface="tf")
+def qcircuit(inputs, theta):
+	fmap.get_circuit(nqubits, inputs[0:4])
+	vform.get_circuit(nqubits, theta[0:12])
+	fmap.get_circuit(nqubits, inputs[4:8])
+	vform.get_circuit(nqubits, theta[12:24])
+	return qml.expval(qml.Hermitian(y, wires=[0]))
+
+def test(theta, data):
+	predicted = []
+	for i in range(len(data)):
+		p0 = prob0(theta, data(i))
+		if (p0 >= .5):
+			predicted.append(0)
+		else:
+			predicted.append(1)
+	return np.array(predicted)
+
+
+
---
+++ vqctf/train.py
@@ -0,0 +1,93 @@
+import pennylane as qml
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import save_model
+
+import os
+import matplotlib.pyplot as plt
+
+from vqctf.circuit import *
+
+import time
+from datetime import datetime
+
+def get_labels(model, data):
+	data = np.array(model(data))
+	for i in range(len(data)):
+		if (i < 0.5):
+			data[i] = 0
+		else:
+			data[i] = 1
+	return data
+
+def get_accuracy(model, data, real):
+	data = get_labels(model, data)
+	return sum(data == real) / len(data)
+
+
+class CustomCallback(keras.callbacks.Callback):
+	def __init__(self, qd):
+		self.qd = qd
+
+	def on_epoch_end(self, epoch, logs=None):
+		
+		train_acc = get_accuracy(self.model, self.qd.train, self.qd.train_nlabels)
+		valid_acc = get_accuracy(self.model, self.qd.validation, self.qd.validation_nlabels)
+		print(f"Epoch {epoch} | Train accuracy: {train_acc} | Valid accuracy: {valid_acc}", flush = True)
+
+
+def train(epochs, lrate, batch_size, qd, name):
+	
+	train_data = qd.train
+	train_labels = qd.train_nlabels
+
+	validation_data = qd.validation
+	validation_labels = qd.validation_nlabels 
+
+	wshape = {"theta": 24}
+
+	start_time = time.time()
+
+	c0layer = tf.keras.layers.Dense(8)
+	qlayer = qml.qnn.KerasLayer(qcircuit, wshape, output_dim=1)
+	clayer = tf.keras.layers.Dense(1, activation="sigmoid")
+	model = tf.keras.models.Sequential([qlayer])
+
+	opt = tf.keras.optimizers.Adam(learning_rate = lrate)
+	model.compile(opt, loss=tf.keras.losses.BinaryCrossentropy())
+	history = model.fit(train_data, train_labels, epochs = epochs, shuffle = True, validation_data = (validation_data, validation_labels), verbose = False, batch_size = batch_size, callbacks = [CustomCallback(qd)])
+
+	end_time = time.time()
+
+	accuracy_train = get_accuracy(model, qd.train, qd.train_nlabels)
+	accuracy_valid = get_accuracy(model, qd.validation, qd.validation_nlabels)
+
+
+	if not (os.path.isdir("vqctf/out")):
+		os.mkdir("vqctf/out")
+	
+	## Logging time!
+	f = open("vqctf/out/log", "a")
+	f.write("VQC " + name + "\n")
+	f.write("Autoencoder: " + qd.model + "\n")
+	f.write(circuit_desc)
+	f.write(f"epochs/lrate/bsize: {epochs} / {lrate} / {batch_size}\n")
+	f.write(f"train/valid {qd.ntrain} / {qd.nvalid}\n")
+	f.write("Elapsed time: " + str(end_time - start_time) + "s " + str((end_time - start_time)/3600) + "h\n")
+	f.write("Train accuracy: " + str(accuracy_train) + "\n")
+	f.write("Valid accuracy: " + str(accuracy_valid) + "\n")
+	f.write("\n\n")
+	f.close();
+
+	os.mkdir("vqctf/out/" + name)
+	model.save_weights("vqctf/out/" + name + "/" + name)
+
+	errs = history.history['loss'];
+	plt.figure();
+	plt.plot(range(len(errs)), errs)
+	plt.title("Loss for " + name)
+	plt.xlabel("Epochs")
+	plt.savefig("vqctf/out/plot-" + name + ".png");
+
+	return model, history

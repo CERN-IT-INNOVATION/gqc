2a15a11651ccca9d75f73c4f1c21a04f3192a241
Samuel Gonzalez Castillo <samuel@sgc.ink>
2021-04-06 16:19:19 +0200

    Implement ang embedding for the vqc

--- vqctf/circuit.py
+++ vqctf/circuit.py
@@ -5,9 +5,12 @@ from vqctf.fmaps import *
 from vqctf.vforms import *
 
 from pennylane.templates import AmplitudeEmbedding
+from pennylane.templates import AngleEmbedding
+
 
 
 state_0 = [[1], [0]]
+state_all = [[1]], 
 y = state_0 * np.conj(state_0).T
 
 def get_layer(spec, nqubits, inputs, theta):
@@ -19,6 +22,10 @@ def get_layer(spec, nqubits, inputs, theta):
 		zzfm(nqubits, inputs[nfrom:nto])
 	elif (name == "zzfm2"):
 		zzfm(nqubits, inputs[nfrom:nto], scaled = True)
+	elif (name == "angle"):
+		AngleEmbedding(features = inputs[nfrom:nto], wires = range(nqubits))
+	elif (name == "angle2"):
+		AngleEmbedding(features = np.pi * inputs[nfrom:nto], wires = range(nqubits))
 	elif (name == "ae"):
 		AmplitudeEmbedding(features = inputs[nfrom:nto], wires = range(nqubits), normalize = True)
 	elif (name == "2local"):
@@ -32,15 +39,30 @@ def get_layer(spec, nqubits, inputs, theta):
 
 
 def get_circuit(spec):
-	nqubits = spec[0]
+	nqubits = 0
+	measure = "1"
+	if (isinstance(spec[0], list)):
+		nqubits = spec[0][0]
+		measure = spec[0][1]
+	else:
+		nqubits = spec[0]
+
 	dev = qml.device("default.qubit", wires=nqubits)
 	
 	@qml.qnode(dev, interface="tf")
 	def qcircuit(inputs, theta):
 		for l in range(1,len(spec)):
 			get_layer(spec[l], nqubits, inputs, theta)
-			
-		return qml.expval(qml.Hermitian(y, wires=[0]))
+		
+		if (measure == "all3"):
+			return qml.expval(qml.Hermitian(y, wires=[0]) @ qml.Hermitian(y,wires = [1]) @ qml.Hermitian(y,wires=[2]))
+		if (measure == "all4"):
+			return qml.expval(qml.Hermitian(y, wires=[0]) @ qml.Hermitian(y,wires = [1]) @ qml.Hermitian(y,wires=[2]) @ qml.Hermitian(y,wires = [3]))
+		elif (measure == "first"):
+			return qml.expval(qml.Hermitian(y, wires = [0]))
+		else:
+			raise Expception("Undefined measurement")
+
 	
 	return qcircuit
 

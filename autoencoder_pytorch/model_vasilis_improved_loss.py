Added improvements up to finalizing the normalization work. Some problematic parts of the code need be fixed before putting the normalisation work to bed.

autoencoder_pytorch/model_vasilis_improved_loss.py
@@ -14,46 +14,43 @@ torch.autograd.profiler.profile(enabled=False)
 class AE(nn.Module):
     # The definition of the model that Vasilis used for the paper.
     # NB: the input layer is included in the node number.
-    def __init__(self, node_number, lr, dropout=False, **kwargs):
+    def __init__(self, nodes, lr, device, **kwargs):
         super(AE, self).__init__()
         self.lr = lr
-        self.node_number = node_number
+        self.nodes  = nodes
+        self.device = device
+        self.beta   = 0.5
+        self.alpha  = 0.5
 
-        self.encoder_layers = self.construct_layers(True, dropout)
+        self.encoder_layers = self.construct_encoder()
         self.encoder = nn.Sequential(*self.encoder_layers)
 
-        self.decoder_layers = self.construct_layers(False, dropout)
+        self.decoder_layers = self.construct_decoder()
         self.decoder = nn.Sequential(*self.decoder_layers)
 
-    def construct_layers(self, encoder, dropout=False):
+    def construct_encoder(self):
         """
-        Construct the layers of the autoencoder module, wheter decoder
-        or encoder.
-
-        @encoder :: Bool, true if encoder, false if decoder.
-        @dropout :: Drop any node in a layer with 20% probability.
-
-        @results :: The layers arrays of the autoencoder.
+        Construct the encoder layers.
         """
-
-        layers = []; decoder = not encoder
-        if encoder: layer_nbs = range(len(self.node_number))
-        if decoder: layer_nbs = reversed(range(len(self.node_number)))
-
+        layers = []
+        layer_nbs = range(len(self.nodes))
         for idx in layer_nbs:
-            if dropout == True:
-                if idx != 0: prob=0.2; layers.append(nn.Dropout(p=prob))
+            layers.append(nn.Linear(self.nodes[idx], self.nodes[idx+1]))
+            layers.append(nn.ELU(True))
+            if idx == len(self.nodes) - 2: layers.append(nn.Sigmoid()); break
 
-            if encoder: layers.append(nn.Linear(self.node_number[idx],
-                self.node_number[idx+1]))
-            else:  layers.append(nn.Linear(self.node_number[idx],
-                self.node_number[idx-1]))
+        return layers
 
-            encoder_last = idx != (len(self.node_number) - 2)
-            decoder_last = idx != 1
-            if encoder and encoder_last: layers.append(nn.ELU(True)); continue
-            if decoder and decoder_last: layers.append(nn.ELU(True)); continue
-            layers.append(nn.Sigmoid()); break
+    def construct_decoder(self):
+        """
+        Construct the decoder layers.
+        """
+        layers = []
+        layer_nbs = reversed(range(len(self.nodes)))
+        for idx in layer_nbs:
+            layers.append(nn.Linear(self.nodes[idx], self.nodes[idx-1]))
+            layers.append(nn.ELU(True))
+            if idx == 1: layers.append(nn.Sigmoid()); break
 
         return layers
 
@@ -63,14 +60,25 @@ class AE(nn.Module):
         reconstructed = self.decoder(latent)
         return reconstructed, latent
 
+    def get_dev(self):   return self.device
     def criterion(self): return nn.MSELoss(reduction='mean')
     def optimizer(self): return optim.Adam(self.parameters(), lr=self.lr)
 
-def eval_valid_loss(model, valid_loader, criterion, min_valid, outdir, device):
-    # Evaluate the model using the validation data. Compute the loss.
-    # If the new loss is a minimum, save this as the best model.
+    def eval_criterion(self, init_feats, recons_out, latent_sig, latent_bkg):
+        # Evaluate the loss function and return its value.
+        criterion_latent = self.criterion()
+        criterion_recons = self.criterion()
+        loss_latent = criterion_latent(latent_sig, latent_bkg)
+        loss_recons = criterion_recons(recons_out, init_feats)
+
+        loss_function = self.alpha*loss_recons + self.beta/loss_latent
+
+        return loss_function
+
+def eval_valid(model, valid_loader, min_valid, outdir):
+    # Evaluate the validation loss for the model and save if got new minimum.
     valid_data_iter = iter(valid_loader)
-    valid_data = valid_data_iter.next().to(device)
+    valid_data = valid_data_iter.next().to(model.get_dev())
     model.eval()
 
     model_output,_ = model(valid_data.float())
@@ -78,43 +86,47 @@ def eval_valid_loss(model, valid_loader, criterion, min_valid, outdir, device):
     if valid_loss < min_valid:
         min_valid = valid_loss
         if outdir is not None:
-            print('New min of loss: {:.2e}'.format(min_valid), flush=True)
+            print('New min of loss: {:.2e}'.format(min_valid))
             torch.save(model.state_dict(), outdir + 'best_model.pt')
 
     return valid_loss, min_valid
 
-def eval_train_loss(model, batch_features, criterion, optimizer, device):
+def eval_train(model, batch_feats, optimizer):
     # Evaluate the training loss.
-    feature_size   = batch_features.shape[1]
-    batch_features = batch_features.view(-1, feature_size).to(device)
-    model_output,_ = model(batch_features.float())
-    train_loss = criterion(model_output, batch_features.float())
+    feature_size  = batch_feats.shape[1]
+    init_feats    = batch_feats.view(-1, feature_size).to(model.get_dev())
+    print(len(init_feats))
+    exit(1)
+    recons_out,_  = model(init_feats.float())
+    _, latent_sig = model(init_feats.float())
+    _, latent_bkg = model(init_feats.float())
+
+    train_loss = model.eval_criterion(init_feats.float(), recons_out,
+        latent_sig, latent_bkg)
     optimizer.zero_grad()
     train_loss.backward()
     optimizer.step()
 
-    return train_loss, optimizer
+    return train_loss
 
-def train(train_loader, valid_loader, model, device, epochs, outdir):
-    # Train the autoencoder that was implemented above.
-    print('Training the Vasilis model...', flush=True)
-    # torch.backends.cudnn.benchmark = True
+def train(train_loader, valid_loader, model, epochs, outdir):
+    # Training method for the autoencoder that was defined above.
+    print('Training the new loss model...')
     loss_training = []; loss_validation = []; min_valid = 99999
     optimizer = model.optimizer()
-    criterion = model.criterion()
 
     for epoch in range(epochs):
         model.train()
-        for i, batch_features in enumerate(train_loader):
-            train_loss, optimizer = eval_train_loss(model, batch_features,
-                criterion, optimizer, device)
-        valid_loss, min_valid = eval_valid_loss(model, valid_loader, criterion,
-            min_valid, outdir, device)
+        for i, batch_feats in enumerate(train_loader):
+            train_loss = eval_train(model, batch_feats, optimizer)
+        valid_loss, min_valid = eval_valid(model, valid_loader, min_valid,
+            outdir)
+
         loss_validation.append(valid_loss)
         loss_training.append(train_loss.item())
         print("Epoch : {}/{}, Training loss (last batch) = {:.8f}".
-               format(epoch + 1, epochs, train_loss.item()), flush=True)
+               format(epoch + 1, epochs, train_loss.item()))
         print("Epoch : {}/{}, Validation loss = {:.8f}".
-               format(epoch + 1, epochs, valid_loss), flush=True)
+               format(epoch + 1, epochs, valid_loss))
 
     return loss_training, loss_validation, min_valid

autoencoder_pytorch/plotting.py
@@ -1,3 +1,5 @@
+# Plot different things given a model, such as the overlaid reconstruction
+# and input variables overlaid.
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse, os
@@ -6,8 +8,10 @@ import torch.nn as nn
 
 from sklearn.preprocessing import MinMaxScaler
 
-import model_vasilis
-import model_vasilis_tanh
+import model_vasilis as basic_nn
+import model_vasilis_tanh as tanh_nn
+import model_vasilis_improved_loss as new_loss
+
 import util
 from util import compute_model
 
@@ -42,8 +46,7 @@ def main():
 
     # Import the model.
     (layers).insert(0, test_data.shape[1])
-    model = util.load_model(model_vasilis_improved.AE, layers, lr,
-        args.model_path, device)
+    model = util.load_model(tanh_nn.AE, layers, lr, args.model_path, device)
     criterion = model.criterion()
 
     # Compute criterion scores for data.
@@ -98,27 +101,6 @@ def import_hyperparams(model_path):
     print("Learning Rate: ", lr)
 
     return layers, batch, lr
-
-def vasilis_reprod_data():
-    # Reproduces the way Vasilis normalizes data such that it leads to
-    # identical plots.
-
-    data_sig = np.load('/work/deodagiu/qml_data/input_ae/x_data_sig.npy')
-    data_bkg = np.load('/work/deodagiu/qml_data/input_ae/x_data_bkg.npy')
-
-    ntot = 360000
-    data_sig = data_sig[:360000, :]
-    data_bkg = data_bkg[:360000, :]
-    all_data = np.vstack((data_sig, data_bkg))
-    norm_data = MinMaxScaler().fit_transform(all_data)
-    data_sig = norm_data[:360000, :]
-    data_bkg = norm_data[360000:, :]
-
-    ntrain, nvalid, ntest = int(0.8*ntot), int(0.1*ntot), int(0.1*ntot)
-    data_sig = data_sig[ntrain+nvalid:ntot, :]
-    data_bkg = data_bkg[ntrain+nvalid:ntot, :]
-
-    return data_sig, data_bkg
 
 def compute_score(model, loader, criterion, prepend_str, score_file):
     """

autoencoder_pytorch/util.py
@@ -22,7 +22,7 @@ def define_torch_device():
         if len(w): print("\033[93mGPU not available. \033[0m")
 
 
-    print("\033[92mUsing device:\033[0m", device, flush=True)
+    print("\033[92mUsing device:\033[0m", device)
     return device
 
 def to_pytorch_data(data, device, batch_size=None, shuffle=True):
@@ -65,7 +65,7 @@ def get_train_data(training_file, validation_file, batch_size, device):
     print("Validation data size: {:.2e}".format(valid_data.shape[0]))
 
     train_loader = to_pytorch_data(train_data, device, batch_size, True)
-    valid_loader = to_pytorch_data(valid_data, device, batch_size, True)
+    valid_loader = to_pytorch_data(valid_data, device, None, True)
 
     end_time = time.time()
     data_load_time = (end_time - start_time)
@@ -106,7 +106,7 @@ def load_model(model_module, layers, lr, model_path, device):
 
     @returns :: The pytorch model object as trained in the training file.
     """
-    model = model_module(node_number=layers, lr=lr).to(device)
+    model = model_module(nodes=layers, device=device, lr=lr).to(device)
     model.load_state_dict(torch.load(model_path + 'best_model.pt',
         map_location=torch.device('cpu')))
     model.eval()

preprocessing/normalise_mlready_data.py
@@ -5,8 +5,10 @@
 # data sets into training and testing subsamples.
 import argparse
 import os
-
 import numpy as np
+import glob
+import matplotlib.pyplot as plt
+
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.preprocessing import StandardScaler
@@ -27,16 +29,47 @@ parser.add_argument("--data_bkg", type=str, required=True,
 parser.add_argument('--maxdata', type=int, default=-1,
     help='The maximum number of training samples to use.')
 
+def varname(index):
+    # Gets the name of what variable is currently considered based on the index
+    # in the data.
+    jet_feats=["$p_t$","$\\eta$","$\\phi$","Energy","$p_x$","$p_y$","$p_z$",
+        "btag"]
+    jet_nvars=len(jet_feats); num_jets = 7
+    met_feats=["$\\phi$","$p_t$","$p_x$","$p_y$"]
+    met_nvars=len(met_feats)
+    lep_feats=["$p_t$","$\\eta$","$\\phi$","Energy","$p_x$","$p_y$","$p_z$"]
+    lep_nvars=len(lep_feats)
+
+    if (index < jet_nvars * num_jets):
+        jet = index // jet_nvars + 1
+        var = index % jet_nvars
+        varstring = "Jet " + str(jet) + " " + jet_feats[var]
+        return varstring
+    index -= jet_nvars * num_jets;
+
+    if (index < met_nvars):
+        var = index % met_nvars;
+        varstring = "MET " + met_feats[var];
+        return varstring
+    index -= met_nvars;
+
+    if (index < lep_nvars):
+        var = index % lep_nvars
+        varstring = "Lepton " + lep_feats[var]
+        return varstring;
+
+    return None
+
 def split_and_save(data, target, name):
     # Splits a given data set into training, testing, and validation samples
     # that then are saved with corresponding names in .npy files.
+    save_dir = os.path.dirname(args.data_sig) + "/"
     print("Splitting data into training, validation, and testing sets.")
     x_train, x_valid, y_train, y_valid = train_test_split(data, target,
         test_size=0.1, shuffle=True)
     x_train, x_test, y_train, y_test = train_test_split(x_train, y_train,
         test_size=0.111111, shuffle=True)
 
-    save_dir = os.path.dirname(args.data_sig) + "/"
     print("Saving data to: ", save_dir)
     np.save(save_dir + "x_data_" + name + "_train", x_train)
     np.save(save_dir + "x_data_" + name + "_test",  x_test)
@@ -45,6 +78,34 @@ def split_and_save(data, target, name):
     np.save(save_dir + "y_data_" + name + "_train", y_train)
     np.save(save_dir + "y_data_" + name + "_test",  y_test)
     np.save(save_dir + "y_data_" + name + "_valid", y_valid)
+
+def plot_sig_bkg(input_sig, input_bkg, norm_name):
+    # Plot sig and bkg overlaid for a dataset.
+    save_dir = os.path.dirname(args.data_sig) + "/"
+    if not os.path.exists(save_dir + 'sig_bkg_' + norm_name + '_plots/'):
+        os.makedirs(save_dir + 'sig_bkg_' + norm_name + '_plots/')
+
+    plt.rc('xtick', labelsize=23)
+    plt.rc('ytick', labelsize=23)
+    plt.rc('axes', titlesize=25)
+    plt.rc('axes', labelsize=25)
+    plt.rc('legend', fontsize=22)
+
+    for idx in range(input_sig.shape[1]):
+
+        plt.figure(figsize=(12,10))
+        plt.xlim(np.amin(input_bkg[:,idx]), np.amax(input_bkg[:,idx]))
+        plt.hist(x=input_bkg[:,idx], bins=60, alpha=0.8, histtype='step',
+            linewidth=2.5, label='Background', density=True, color='gray',
+            hatch='xx')
+        plt.hist(x=input_sig[:,idx], bins=60, alpha=0.8, histtype='step',
+            linewidth=2.5, label='Signal', density=True, color='navy')
+        plt.xlabel(varname(idx)); plt.ylabel('Density')
+        plt.gca().set_yscale("log")
+        plt.legend()
+        plt.savefig(save_dir + 'sig_bkg_' + norm_name + '_plots/' +
+            varname(idx) + '.pdf')
+        plt.close()
 
 def apply_norm(norm_method, norm_name, data, target, maxdata):
     """
@@ -57,15 +118,22 @@ def apply_norm(norm_method, norm_name, data, target, maxdata):
     @target      :: The corresponding target array for the data.
     @base_fn     :: The base filename of the raw data.
     """
-    data_norm = norm_method().fit_transform(data)
-    split_and_save(data_norm, target, norm_name + "_{:.2e}".format(maxdata))
+    norm_name = norm_name + "_{:.2e}".format(maxdata)
+    if check_norm_exists(norm_name): return
+    data_norm = norm_method.fit_transform(data)
+    sig_mask = (target == 1); bkg_mask = (target == 0)
+    plot_sig_bkg(data_norm[sig_mask, :], data_norm[bkg_mask, :], norm_name)
+    split_and_save(data_norm, target, norm_name)
 
 def normalize_jet(data, jet_nb):
     # Normalize the jet features for a specific jet. For optimal normalization.
     pt = 0 + jet_nb*8; eta = pt+1;
     en = 3 + jet_nb*8; phi = en  ; px = en+1; pz = en+4;
-    data[:,pt] = MinMaxScaler().fit_transform(data[:,pt].reshape(-1, 1))[:,0]
-    data[:,en] = MinMaxScaler().fit_transform(data[:,en].reshape(-1, 1))[:,0]
+    power_trans = PowerTransformer(method='box-cox')
+    data[:,pt][data[:,pt] == 0] = 1e-8
+    data[:,en][data[:,en] == 0] = 1e-8
+    data[:,pt]  = MinMaxScaler().fit_transform(data[:,pt].reshape(-1, 1))[:,0]
+    data[:,en]  = MinMaxScaler().fit_transform(data[:,en].reshape(-1, 1))[:,0]
     data[:,eta:phi] = MaxAbsScaler().fit_transform(data[:,pt+1:en])
     data[:,px:pz]   = MaxAbsScaler().fit_transform(data[:,en+1:en+4])
 
@@ -74,6 +142,7 @@ def normalize_jet(data, jet_nb):
 def normalize_met(data):
     # Normalize the metadata features. For the optimal normalization.
     phi = 7*8; pt = phi+1; px=phi+2; py=phi+3;
+    power_trans = PowerTransformer(method='box-cox')
     data[:,phi] = MaxAbsScaler().fit_transform(data[:,phi].reshape(-1,1))[:,0]
     data[:,pt]  = MinMaxScaler().fit_transform(data[:,pt].reshape(-1,1))[:,0]
     data[:,px]  = MaxAbsScaler().fit_transform(data[:,px].reshape(-1,1))[:,0]
@@ -85,6 +154,7 @@ def normalize_lep(data):
     # Normalize the lepton features. For the optimal normalization.
     pt = 7*8+4; eta = pt+1; phi= pt+2; en = pt+3
     px = pt+4; py = pt+5; pz = pt+6
+    power_trans = PowerTransformer(method='box-cox')
     data[:,pt]  = MinMaxScaler().fit_transform(data[:,pt].reshape(-1,1))[:,0]
     data[:,eta] = MaxAbsScaler().fit_transform(data[:,eta].reshape(-1,1))[:,0]
     data[:,phi] = MaxAbsScaler().fit_transform(data[:,phi].reshape(-1,1))[:,0]
@@ -95,13 +165,24 @@ def normalize_lep(data):
 
     return data
 
-def apply_optimal_normalization(data, target, maxdata):
+def apply_optimal_norm(data, target, maxdata):
     # Apply normalization specific to the ttHbb data that we are analyzing.
+    norm_name = "optimal_norm_{:.2e}".format(maxdata)
+    if check_norm_exists(norm_name): return
     data_norm = data
     for jet_nb in range(8): data_norm = normalize_jet(data_norm, jet_nb)
     data_norm = normalize_met(data_norm)
     data_norm = normalize_lep(data_norm)
-    split_and_save(data_norm, target, "optimal_norm_{:.2e}".format(maxdata))
+    sig_mask = (target == 1); bkg_mask = (target == 0)
+    plot_sig_bkg(data_norm[sig_mask, :], data_norm[bkg_mask, :], norm_name)
+    split_and_save(data_norm, target, norm_name)
+
+def check_norm_exists(norm_name):
+    if glob.glob(os.path.dirname(args.data_sig) + '/*' + norm_name + "*.npy"):
+        print("\033[93mWarning: Files with name ∋ \"" + norm_name + "\" "
+              "exists so not producing it again.\033[0m")
+        return 1
+    return 0
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -113,9 +194,16 @@ if __name__ == "__main__":
     all_data = np.vstack((data_sig, data_bkg))
     all_targ = np.concatenate((np.ones(args.maxdata), np.zeros(args.maxdata)))
 
-    print("\n\033[92mApplying optimal normalization...\033[0m")
-    apply_optimal_normalization(all_data, all_targ, args.maxdata)
     print("\n\033[92mApplying minmax normalization...\033[0m")
-    apply_norm(MinMaxScaler, "minmax", all_data, all_targ, args.maxdata)
+    apply_norm(MinMaxScaler(), "minmax_norm", all_data, all_targ, args.maxdata)
+    print("\n\033[92mApplying standard normalization...\033[0m")
+    apply_norm(StandardScaler(), "std_norm", all_data, all_targ, args.maxdata)
+    print("\n\033[92mApplying quantile normalization...\033[0m")
+    apply_norm(QuantileTransformer(), "quant_norm", all_data, all_targ,
+        args.maxdata)
+    print("\n\033[92mApplying quantile gauss normalization...\033[0m")
+    apply_norm(QuantileTransformer(output_distribution='normal'),"qgauss_norm",
+        all_data, all_targ, args.maxdata)
 
-
+    print("\n\033[92mApplying optimal normalization...\033[0m")
+    apply_optimal_norm(all_data, all_targ, args.maxdata)
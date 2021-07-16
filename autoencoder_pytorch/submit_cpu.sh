22b9af15e27398485390f2b7459cb33d845c6db9
bb511 <patrickodagiu@gmail.com>
2021-07-16 16:28:49 +0200

    Updated the gpu and cpu submit scripts for the training.

--- autoencoder_pytorch/submit_cpu.sh
+++ autoencoder_pytorch/submit_cpu.sh
@@ -3,27 +3,27 @@
 #SBATCH --mem=3000M
 #SBATCH -o ./logs/ae_train_%j.log
 
-usage() { echo "Usage: $0 [-t <training_file_path>] [-v <validation_file_path>] [-l <learning_rate>] [-b <batch_size>] [-e <number_of_epochs>] [-f <file_flag>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-n <normalization_name>] [-s <number_of_events>] [-l <learning_rate>] [-b <batch_size>] [-e <number_of_epochs>] [-f <file_flag>]" 1>&2; exit 1; }
 
-while getopts ":t:v:l:b:e:s:f:" o; do
+while getopts ":n:s:l:b:e:f:" o; do
     case "${o}" in
-    t)
-        t=${OPTARG}
+    n)
+        n=${OPTARG}
             ;;
-    v)
-        v=${OPTARG}
+    s)
+        s=${OPTARG}
             ;;
-	l)
-	    l=${OPTARG}
+    l)
+	l=${OPTARG}
 	    ;;
-	b)
-	    b=${OPTARG}
+    b)
+        b=${OPTARG}
 	    ;;
-	e)
-	    e=${OPTARG}
+    e)
+	e=${OPTARG}
 	    ;;
-	f)
-	    f=${OPTARG}
+    f)
+	f=${OPTARG}
 	    ;;
     *)
         usage
@@ -32,15 +32,15 @@ while getopts ":t:v:l:b:e:s:f:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${t}" ] || [ -z "${v}" ] || [ -z "${l}" ] || [ -z "${b}" ] || [ -z "${e}" ] || [ -z "${f}" ]; then
+if [ -z "${n}" ] || [ -z "${s}" ] || [ -z "${l}" ] || [ -z "${b}" ] || [ -z "${e}" ] || [ -z "${f}" ]; then
     usage
 fi
 
 echo "--------------------------------------- "
 echo "The hyperparameters of this run are:"
 echo " "
-echo "Input training file: ${t}"
-echo "Input validation file: ${v}"
+echo "Normalization used in data file: ${n}"
+echo "Number of events: ${s}"
 echo "Learning rate: ${l}"
 echo "Batch size: ${b}"
 echo "Number of Epochs: ${e}"
@@ -50,5 +50,5 @@ echo "--------------------------------------- "
 
 source /work/deodagiu/miniconda3/bin/activate qml_project
 export PYTHONUNBUFFERED=TRUE
-python3 train.py --data_folder /work/deodagiu/qml_data/input_ae/ --train_file ${t} --valid_file ${v} --lr ${l} --batch ${b} --epochs ${e} --file_flag ${f}
+python3 train.py --data_folder /work/deodagiu/qml_data/input_ae/ --norm ${n} --nevents ${s} --lr ${l} --batch ${b} --epochs ${e} --file_flag ${f}
 export PYTHONUNBUFFERED=FALSE
--- autoencoder_pytorch/submit_gpu.sh
+++ autoencoder_pytorch/submit_gpu.sh
@@ -8,15 +8,15 @@
 #SBATCH -o ./logs/ae_train_gpu_%j.log
 
 mkdir -p ./logs
-usage() { echo "Usage: $0 [-t <training_file_path>] [-v <validation_file_path>] [-l <learning_rate>] [-b <batch_size>] [-e <number_of_epochs>] [-f <file_flag>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-n <normalization_name>] [-s <number_of_events>] [-l <learning_rate>] [-b <batch_size>] [-e <number_of_epochs>] [-f <file_flag>]" 1>&2; exit 1; }
 
-while getopts ":t:v:l:b:e:s:f:" o; do
+while getopts ":n:s:l:b:e:f:" o; do
     case "${o}" in
-    t)
-        t=${OPTARG}
+    n)
+        n=${OPTARG}
         ;;
-    v)
-        v=${OPTARG}
+    s)
+        s=${OPTARG}
         ;;
     l)
         l=${OPTARG}
@@ -37,15 +37,15 @@ while getopts ":t:v:l:b:e:s:f:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${t}" ] || [ -z "${v}" ] || [ -z "${l}" ] || [ -z "${b}" ] || [ -z "${e}" ] || [ -z "${f}" ]; then
+if [ -z "${n}" ] || [ -z "${s}" ] || [ -z "${l}" ] || [ -z "${b}" ] || [ -z "${e}" ] || [ -z "${f}" ]; then
     usage
 fi
 
 echo "--------------------------------------- "
 echo "The hyperparameters of this run are:"
 echo " "
-echo "Input training file: ${t}"
-echo "Input validation file: ${v}"
+echo "Normalization of the input file: ${n}"
+echo "Number of events in the input file: ${s}"
 echo "Learning rate: ${l}"
 echo "Batch size: ${b}"
 echo "Number of Epochs: ${e}"
@@ -55,5 +55,5 @@ echo "--------------------------------------- "
 
 source /work/deodagiu/miniconda3/bin/activate qml_project
 export PYTHONUNBUFFERED=TRUE
-python3 train.py --data_folder /work/deodagiu/qml_data/input_ae/ --train_file ${t} --valid_file ${v} --lr ${l} --batch ${b} --epochs ${e} --file_flag ${f}
+python3 train.py --data_folder /work/deodagiu/qml_data/input_ae/ --norm ${n} --nevents ${s} --lr ${l} --batch ${b} --epochs ${e} --file_flag ${f}
 export PYTHONUNBUFFERED=FALSE

8ccbc63e37cccf68590b06555ecbc2b1da559bff
bb511 <patrickodagiu@gmail.com>
2021-11-10 17:26:58 +0100

    Made the preprocessing files flake8 compliant.

--- preprocessing/normalise_mlready_data.py
+++ preprocessing/normalise_mlready_data.py
@@ -5,7 +5,9 @@
 # sklearn is used to normalize the data sets. Finally, the normalised data
 # is split into training, validation, and testing subsets.
 
-import argparse, os, glob
+import argparse
+import os
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,10 +15,8 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import MaxAbsScaler
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import RobustScaler
-from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.preprocessing import PowerTransformer
-from sklearn.preprocessing import minmax_scale
 
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
@@ -24,22 +24,26 @@ from sklearn import metrics
 from terminal_colors import tcols
 
 parser = argparse.ArgumentParser(formatter_class=argparse.
-    ArgumentDefaultsHelpFormatter)
+                                 ArgumentDefaultsHelpFormatter)
 parser.add_argument("--data_sig", type=str, required=True,
-    help="Path to the .npy file containing sig data to be normalized.")
+                    help="Path to the .npy file containing sig data.")
 parser.add_argument("--data_bkg", type=str, required=True,
-    help="Path to the .npy file containing bkg data to be normalized.")
+                    help="Path to the .npy file containing bkg data.")
 parser.add_argument("--norm_name", type=str, required=True,
-    help="Choose which way to normalise the data."
-         "Available options: vanilla (no norm), minmax, maxabs, std, robust, "
-         "power, quantile.")
+                    help="Choose which way to normalise the data."
+                         "Available options: vanilla (no norm), minmax, "
+                         "maxabs, std, robust, power, quantile.")
 parser.add_argument("--maxdata", type=int, default=-1,
-    help="The maximum train samples to use for signal. bkg will be equal.")
+                    help="The maximum train samples to use for signal. "
+                         "bkg will be equal.")
 parser.add_argument("--valid_percent", type=float, default=-1,
-    help="Percentage of total data that will make validation and test data,\
-          These two data sets will be equal in size.\
-          Example: if you give 0.1 then 10\% of the data will be validation,\
-          10\% of the data will be test, and 80\% will be training.")
+                    help="Percentage of total data that will make "
+                    "validation and test data."
+                    "These two data sets will be equal in size."
+                    "Example: if you give 0.1 then 10\% of the data will"
+                    "be validation, 10\% of the data will be test, and 80\%"
+                    "will be training.")
+
 
 def main():
     print(tcols.HEADER)
@@ -55,10 +59,11 @@ def main():
 
     choose_norm(args.norm_name, all_data, all_targ)
 
+
 def choose_norm(norm_name, all_data, all_targ):
     """
     Normalise the mlready data.
-    @norm_name :: Name of the normalisation to be applied, chosen by user.
+    @norm_name :: Name of the normalisation applied, chosen by user.
     """
     print(tcols.OKBLUE)
     if norm_name == "vanilla":
@@ -67,55 +72,61 @@ def choose_norm(norm_name, all_data, all_targ):
     elif norm_name == "minmax":
         print("\nApplying minmax normalization...")
         apply_norm(MinMaxScaler(), f"minmax_{args.maxdata:.2e}",
-            all_data, all_targ)
+                   all_data, all_targ)
     elif norm_name == "maxabs":
         print("\nApplying maxabs normalization...")
         apply_norm(MaxAbsScaler(), f"maxabs_{args.maxdata:.2e}",
-            all_data, all_targ)
+                   all_data, all_targ)
     elif norm_name == "std":
         print("\nApplying standard normalization...")
         apply_norm(StandardScaler(), f"std_{args.maxdata:.2e}",
-            all_data, all_targ)
+                   all_data, all_targ)
     elif norm_name == "robust":
         print("\nApplying robust normalization...")
         apply_norm(RobustScaler(), f"robust_{args.maxdata:.2e}",
-            all_data, all_targ)
+                   all_data, all_targ)
     elif norm_name == "power":
         print("\nApplying power normalization...")
         apply_norm(PowerTransformer(), f"power_{args.maxdata:.2e}",
-            all_data, all_targ)
+                   all_data, all_targ)
     elif norm_name == "quantile":
         print("\nApplying quantile normalization...")
         apply_norm(QuantileTransformer(), f"quantile_{args.maxdata:.2e}",
-            all_data, all_targ)
+                   all_data, all_targ)
     else:
         print(tcols.ENDC)
         raise TypeError("Specified normalisation type does not exist!!!")
 
     print(tcols.ENDC)
 
+
 def apply_norm(norm_method, norm_name, data, target):
     """
-    Apply a specific normalization to the whole data set. Save the normalized
-    data set into a separate numpy array specifying the norm name.
+    Apply a specific normalization to the whole data set. Save the
+    normalized data set into a separate numpy array specifying the
+    norm name.
     @norm_method :: The normalization method sklearn object.
     @norm_name   :: String of the normalization name.
     @data        :: 2D numpy array of the features to be normalized.
-    @target      :: 1D numpy array with 0 for each bkg event and 1 for the sig.
+    @target      :: 1D numpy array with 0 for each bkg event and 1 for
+                    the sig.
     """
-    if check_norm_exists(norm_name): return
+    if check_norm_exists(norm_name):
+        return
     data_norm = norm_method.fit_transform(data)
     plot_roc_auc(data_norm, target, norm_name)
-    sig_mask = (target == 1); bkg_mask = (target == 0)
+    sig_mask = (target == 1)
+    bkg_mask = (target == 0)
     plot_sig_bkg(data_norm[sig_mask, :], data_norm[bkg_mask, :], norm_name)
     split_and_save(data_norm, target, norm_name)
 
+
 def split_and_save(data, target, name):
     """
-    Splits a given data set into training, testing, and validation samples
-    that then are saved with corresponding names in .npy files.
+    Splits a given data set into training, testing, and validation
+    samples that then are saved with corresponding names in .npy files.
     @data   :: 2D numpy array of the features to be normalized.
-    @target :: 1D numpy array with 0 for each bkg event and 1 for the sig.
+    @target :: 1D numpy array with 0 for each bkg event and 1 for sig.
     @name   :: String containing the name of the normalisation and the
         number of total signal/background events in the data object.
 
@@ -125,11 +136,13 @@ def split_and_save(data, target, name):
     print("Splitting data into training, validation, and testing sets.")
     print(tcols.ENDC)
 
-    x_train, x_valid, y_train, y_valid = train_test_split(data, target,
-        test_size=args.valid_percent, shuffle=True, stratify=target)
+    x_train, x_valid, y_train, y_valid = \
+        train_test_split(data, target, test_size=args.valid_percent,
+                         shuffle=True, stratify=target)
     test_percent = float(x_valid.shape[0]/x_train.shape[0])
-    x_train, x_test, y_train, y_test = train_test_split(x_train, y_train,
-        test_size=test_percent, shuffle=True, stratify=y_train)
+    x_train, x_test, y_train, y_test = \
+        train_test_split(x_train, y_train, test_size=test_percent,
+                         shuffle=True, stratify=y_train)
 
     print(tcols.OKGREEN)
     print("Saving data to: ", save_dir)
@@ -142,15 +155,17 @@ def split_and_save(data, target, name):
     np.save(save_dir + "y_data_" + name + "_test",  y_test)
     np.save(save_dir + "y_data_" + name + "_valid", y_valid)
 
+
 def plot_sig_bkg(input_sig, input_bkg, norm_name):
     """
-    Plot a histogram for the signal normalized data and a histogram for the
-    background normalized data and then overlay them.
+    Plot a histogram for the signal normalized data and a histogram
+    for the background normalized data and then overlay them.
     @input_sig :: 2D numpy array containing the signal data.
     @input_bkg :: 2D numpy array containing the background data.
     @norm_name :: String name of the normalization that was applied.
 
-    returns :: Saves the plots in a folder where the signal data is located.
+    returns :: Saves the plots in a folder where the signal data is
+        located.
     """
     save_dir = os.path.dirname(args.data_sig) + "/"
     sig_bkg_plot_dir = save_dir + 'sig_bkg_' + norm_name + '_plots/'
@@ -165,24 +180,28 @@ def plot_sig_bkg(input_sig, input_bkg, norm_name):
 
     for idx in range(input_sig.shape[1]):
 
-        plt.figure(figsize=(12,10))
-        plt.xlim(np.amin(input_bkg[:,idx]), np.amax(input_bkg[:,idx]))
-        plt.hist(x=input_bkg[:,idx], bins=60, alpha=0.8, histtype='step',
-            linewidth=2.5, label='Background', density=True, color='gray',
-            hatch='xx')
-        plt.hist(x=input_sig[:,idx], bins=60, alpha=0.8, histtype='step',
-            linewidth=2.5, label='Signal', density=True, color='navy')
-        plt.xlabel(varname(idx)); plt.ylabel('Density')
+        plt.figure(figsize=(12, 10))
+        plt.xlim(np.amin(input_bkg[:, idx]), np.amax(input_bkg[:, idx]))
+        plt.hist(x=input_bkg[:, idx], bins=60, alpha=0.8, histtype='step',
+                 linewidth=2.5, label='Background', density=True, color='gray',
+                 hatch='xx')
+        plt.hist(x=input_sig[:, idx], bins=60, alpha=0.8, histtype='step',
+                 linewidth=2.5, label='Signal', density=True, color='navy')
+        plt.xlabel(varname(idx))
+        plt.ylabel('Density')
         plt.gca().set_yscale("log")
         plt.legend()
         plt.savefig(sig_bkg_plot_dir + varname(idx) + '.pdf')
         plt.close()
 
+
 def plot_roc_auc(data, target, norm_name):
     """
     Compute the roc curve of a given 2D dataset of features.
-    @data      :: 2D array, each column is a feature and each row an event.
-    @target    :: 1D array, each element is 0 or 1 corresponding to bkg or sig.
+    @data      :: 2D array, each column is a feature and each row an
+                  event.
+    @target    :: 1D array, each element is 0 or 1 corresponding to bkg
+                  or sig.
     @norm_name :: String of the name of the normalization.
 
     returns :: Prints and saves the the roc curve along with an indication
@@ -209,7 +228,8 @@ def plot_roc_auc(data, target, norm_name):
         plt.plot(fpr, tpr, label=f"AUC: {auc}", color='navy')
         plt.plot([0, 1], [0, 1], ls="--", color='gray')
 
-        plt.xlim([0.0, 1.0]); plt.ylim([0.0, 1.0])
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.0])
         plt.legend()
 
         auc_sum += auc
@@ -219,10 +239,11 @@ def plot_roc_auc(data, target, norm_name):
     with open(roc_auc_plot_dir + 'auc_sum.txt', 'w') as auc_sum_file:
         auc_sum_file.write(f"{auc_sum:.3f}")
 
+
 def check_norm_exists(norm_name):
     """
-    Quick helper method that checks if the normalise data files already  exists
-    and returns 1 if it does but 0 if it does not.
+    Quick helper method that checks if the normalise data files already
+    exists and returns 1 if it does but 0 if it does not.
     @norm_name :: The name of the normalisation.
     """
     if glob.glob(os.path.dirname(args.data_sig) + '/*' + norm_name + "*.npy"):
@@ -231,42 +252,45 @@ def check_norm_exists(norm_name):
         return 1
     return 0
 
+
 def varname(index):
     """
-    The variables in the currently analysed arrays follow a certain order.
-    This method gets the name of the variable given its index in the data.
+    The variables in the currently analysed arrays follow an order.
+    This method gets the name of the variable given its data index.
     @index :: The index for which the name is returned.
 
     returns :: None if the index is not valid or string of the variable name
         if the given index is valid.
     """
-    jet_feats=["$p_t$","$\\eta$","$\\phi$","Energy","$p_x$","$p_y$","$p_z$",
-        "btag"]
-    jet_nvars=len(jet_feats); num_jets = 7
-    met_feats=["$\\phi$","$p_t$","$p_x$","$p_y$"]
-    met_nvars=len(met_feats)
-    lep_feats=["$p_t$","$\\eta$","$\\phi$","Energy","$p_x$","$p_y$","$p_z$"]
-    lep_nvars=len(lep_feats)
+    jet_feats = ["$p_t$", "$\\eta$", "$\\phi$", "Energy", "$p_x$", "$p_y$",
+                 "$p_z$", "btag"]
+    jet_nvars = len(jet_feats); num_jets = 7
+    met_feats = ["$\\phi$", "$p_t$", "$p_x$", "$p_y$"]
+    met_nvars = len(met_feats)
+    lep_feats = ["$p_t$", "$\\eta$", "$\\phi$", "Energy", "$p_x$", "$p_y$",
+                 "$p_z$"]
+    lep_nvars = len(lep_feats)
 
     if (index < jet_nvars * num_jets):
         jet = index // jet_nvars + 1
         var = index % jet_nvars
         varstring = "Jet " + str(jet) + " " + jet_feats[var]
         return varstring
-    index -= jet_nvars * num_jets;
+    index -= jet_nvars * num_jets
 
     if (index < met_nvars):
-        var = index % met_nvars;
-        varstring = "MET " + met_feats[var];
+        var = index % met_nvars
+        varstring = "MET " + met_feats[var]
         return varstring
-    index -= met_nvars;
+    index -= met_nvars
 
     if (index < lep_nvars):
         var = index % lep_nvars
         varstring = "Lepton " + lep_feats[var]
-        return varstring;
+        return varstring
 
     return None
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
--- preprocessing/prepare_mlready_data.py
+++ preprocessing/prepare_mlready_data.py
@@ -4,40 +4,43 @@
 # formats it, and casts it into a flat 2D array that is saved in the
 # x_data_ray.npy file.
 
-import argparse, os, glob, itertools
+import argparse
+import os
 import pandas as pd
 import numpy as np
 
 from terminal_colors import tcols
 
-# Disable the pandas warning when working on a copy. Re-enable if you care
-# about what you do to a dataframe copy making it back to original.
+# Disable the pandas warning when working on a copy. Re-enable if you
+# care about what you do to a dataframe copy making it back to original.
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--sig_file", type=str, required=True, action="store",
-    help="The name of the signal .h5 file (including the extension).")
+                    help="The name of the signal .h5 file.")
 parser.add_argument("--bkg_file", type=str, required=True, action="store",
-    help="The name of the background .h5 file (including the extension).")
+                    help="The name of the background .h5 file.")
 parser.add_argument("--outdir", type=str, action="store",
-    default="./ml_ready_unnormalised_data/",
-    help="The output directory.")
+                    default="./ml_ready_unnormalised_data/",
+                    help="The output directory name.")
 
 # Variables of choice and selection cuts on some of these variables.
 # Consistent with https://arxiv.org/abs/2104.07692.
-njets     = 7
-nleps     = 1
+njets = 7
+nleps = 1
 jet_feats = ["pt", "eta", "phi", "en", "px", "py", "pz", "btag"]
-lep_feats = ["pt","eta","phi","en","px","py","pz"]
+lep_feats = ["pt", "eta", "phi", "en", "px", "py", "pz"]
 met_feats = ["phi", "pt", "px", "py"]
 selection = "nleps == 1 & (nbtags >= 2) & (njets >= 4)"
+
 
 def main():
     data_sig = read_single_file(args.sig_file, "1000000")
     data_bkg = read_single_file(args.bkg_file, "1000000")
     print(tcols.OKGREEN + "Data is loaded!" + tcols.ENDC)
-    if not os.path.exists(args.outdir): os.makedirs(args.outdir)
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
 
     sig, y_sig = make_flat_numpy_array(data_sig)
     bkg, y_bkg = make_flat_numpy_array(data_bkg, False)
@@ -45,29 +48,31 @@ def main():
     np.save(os.path.join(args.outdir, "x_data_sig"), sig)
     np.save(os.path.join(args.outdir, "x_data_bkg"), bkg)
 
+
 def read_single_file(path, chunksize):
     """
     Load a single .h5 file with a certain chunksize.
     @path       :: String path to the data.
-    @chunksize  :: String of the number of rows imported at once into RAM.
+    @chunksize  :: String of the number of rows imported into RAM.
 
     returns :: The loaded Pandas data frame object.
     """
     print(f"Loading h5 file {path}...")
     return pd.read_hdf(path, chunksize=chunksize)
 
-def make_flat_numpy_array(data, is_signal=True):
+
+def make_flat_numpy_array(data, is_signal=True) -> np.ndarray:
     """
     Take the loaded .h5 dataset and save the features of interest.
 
     @data      :: The .h5 imported data.
-    @is_signal :: Bool of whether the data is signal (True) or background.
+    @is_signal :: Bool of whether the data is signal or background.
 
-    @returns   :: A 2D python list containing the events (rows) and features
-        (columns) pre-selected and formatted.
+    @returns   :: A 2D python list containing the events (rows) and
+        features (columns) pre-selected and formatted.
     """
     chunk_nb = 0
-    flats    = [np.array([]), np.array([]), np.array([])]
+    flats = [np.array([]), np.array([]), np.array([])]
     for chunk in data:
         chunk_nb += 1
         print(tcols.HEADER)
@@ -75,14 +80,19 @@ def make_flat_numpy_array(data, is_signal=True):
         print(tcols.ENDC)
         chunk = map_jet_btag_values(chunk)
 
-        if selection is not None: chunk = chunk.query(selection)
-        if jet_feats is not None: flats = jet_formatting(chunk, flats)
-        if met_feats is not None: flats = met_formatting(chunk, flats)
-        if lep_feats is not None: flats = lep_formatting(chunk, flats)
+        if selection is not None:
+            chunk = chunk.query(selection)
+        if jet_feats is not None:
+            flats = jet_formatting(chunk, flats)
+        if met_feats is not None:
+            flats = met_formatting(chunk, flats)
+        if lep_feats is not None:
+            flats = lep_formatting(chunk, flats)
 
     return make_flat_features(flats, is_signal)
 
-def map_jet_btag_values(data):
+
+def map_jet_btag_values(data) -> np.ndarray:
     """
     Map the jet btag from being 0-10 to being 0 when there are no jets
     and being one when there are any number of jets.
@@ -96,33 +106,40 @@ def map_jet_btag_values(data):
 
     return data
 
-def jet_formatting(data, flats):
+
+def jet_formatting(data, flats) -> np.ndarray:
     """
     Formatting the jets features.
     @data  :: Pandas data frame object containing the data.
-    @flats :: Array containing the values of the different data features in 2D.
+    @flats :: Array containing the values of the different data
+              features in 2D.
 
     returns :: The updated flats array.
     """
     print(tcols.OKBLUE + "Formatting jets..." + tcols.ENDC)
 
-    onejet  = list(range(njets)); number_jet_feats = len(jet_feats)
-    jet_col = ["jets_%s_%d"%(feat,jet) for jet in onejet for feat in jet_feats]
-    jetsa   = data[jet_col].values
+    onejet = list(range(njets))
+    number_jet_feats = len(jet_feats)
+    jetc = ["jets_%s_%d" % (feat, jet) for jet in onejet for feat in jet_feats]
+    jetsa = data[jetc].values
 
-    if flats[0].size == 0:   flats[0] = jetsa
-    else:                    flats[0] = np.concatenate((flats[0], jetsa))
+    if flats[0].size == 0:
+        flats[0] = jetsa
+    else:
+        flats[0] = np.concatenate((flats[0], jetsa))
 
     jetsa = jetsa.reshape(-1, njets, number_jet_feats)
     print("Jet formatting done. Shape of the array: ", jetsa.shape)
 
     return flats
 
-def met_formatting(data, flats):
+
+def met_formatting(data, flats) -> np.ndarray:
     """
     Formatting the meta features.
     @data  :: Pandas data frame object containing the data.
-    @flats :: Array containing the values of the different data features in 2D.
+    @flats :: Array containing the values of the different data
+              features in 2D.
 
     returns :: The updated flats array.
     """
@@ -133,56 +150,66 @@ def met_formatting(data, flats):
     data["met_py"] = data["met_" + met_feats[1]] * \
         np.sin(data["met_"+met_feats[0]])
     meta = data[["met_%s" % feat for feat in met_feats]].values
-    if flats[1].size == 0:  flats[1] = meta
-    else:                   flats[1] = np.concatenate((flats[1], meta))
+    if flats[1].size == 0:
+        flats[1] = meta
+    else:
+        flats[1] = np.concatenate((flats[1], meta))
     print("Missing energy formatting done. Shape of the array:", meta.shape)
 
     return flats
 
-def lep_formatting(data, flats):
+
+def lep_formatting(data, flats) -> np.ndarray:
     """
     Formatting the lepton features.
     @data  :: Pandas data frame object containing the data.
-    @flats :: Array containing the values of the different data features in 2D.
+    @flats :: Array containing the values of the different data
+              features in 2D.
 
     returns :: The updated flats array.
     """
     print(tcols.OKBLUE + "Formatting leptons..." + tcols.ENDC)
 
     number_lep_feats = len(lep_feats)
-    lepsa = data[["leps_%s_%d" % (feat,lep) for lep in range(nleps)
-        for feat in lep_feats]].values
-    if flats[2].size == 0:   flats[2] = lepsa
-    else:                    flats[2] = np.concatenate((flats[2], lepsa))
+    lepsa = data[["leps_%s_%d" % (feat, lep) for lep in range(nleps)
+                  for feat in lep_feats]].values
+    if flats[2].size == 0:
+        flats[2] = lepsa
+    else:
+        flats[2] = np.concatenate((flats[2], lepsa))
     lepsa = lepsa.reshape(-1, nleps, number_lep_feats)
     print("Lepton formatting done. Shape of the array:", lepsa.shape)
 
     return flats
 
-def make_flat_features(flats, is_signal=True):
+
+def make_flat_features(flats, is_signal=True) -> np.ndarray:
     """
     Make a 2D flat array of all the selected features, with a row per event
     and the columns being each features (variable).
     @flats     :: 3D array, with each class of features being a 2D array.
     @is_signal :: Bool of whether we are dealing with sig or bkg events.
 
-    returns :: A 2D matrix, containing rows for each event and columns for
-        each feature selected to be part of the final data set.
-        Also returns a 1D array filled with 0s for each bkg events and 1s for
-        each signal event. The position of the 0 or 1 matches the row number
-        in the 2D array mentioned earlier.
+    returns :: A 2D matrix, containing rows for each event and columns
+        for each feature selected to be part of the final data set.
+        Also returns a 1D array filled with 0s for each bkg events and
+        1s for each signal event. The position of the 0 or 1 matches
+        the row number in the 2D array mentioned earlier.
     """
     print("\n-----------\n\n\n")
     print(tcols.OKGREEN + "Making flat features..." + tcols.ENDC)
     flat_array = np.hstack(flats)
 
     nevents = flat_array.shape[0]
-    if is_signal: y = np.ones(nevents)
-    else:         y = np.zeros(nevents)
+    if is_signal:
+        y = np.ones(nevents)
+    else:
+        y = np.zeros(nevents)
 
     print(tcols.OKGREEN + "Done!" + tcols.ENDC)
 
     return flat_array, y
+
 
 if __name__ == "__main__":
     args = parser.parse_args()

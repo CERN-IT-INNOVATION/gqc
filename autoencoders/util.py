a36cccfdc154e67b971f3cb56e73bb66b9808843
Patrick Odagiu <podagiu@student.ethz.ch>
2022-05-05 17:01:24 +0200

    Added auto-assignment of gpus to auto-encoder training.

--- autoencoders/util.py
+++ autoencoders/util.py
@@ -3,6 +3,9 @@ import torch
 
 import warnings
 import json
+import os
+import subprocess
+import time
 
 from .ae_vanilla import AE_vanilla
 from .ae_classifier import AE_classifier
@@ -41,13 +44,48 @@ def define_torch_device() -> torch.device:
     print("\n")
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter("always")
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        device = torch.device("cuda:" + str(get_free_gpu()) if
+            torch.cuda.is_available() else "cpu")
         if len(w):
             print(tcols.WARNING + "GPU not available." + tcols.ENDC)
 
     print("\033[92mUsing device:\033[0m", device)
     return device
 
+def get_free_gpu(threshold_vram_usage=3000, max_gpus=1):
+    """
+    Returns the free gpu numbers on your system, to replace x in the string 'cuda:x'.
+    The freeness is determined based on how much memory is currently being used on a
+    gpu.
+
+    Args:
+        threshold_vram_usage: A GPU is considered free if the vram usage is below the
+            threshold.
+        max_gpus: Max GPUs is the maximum number of gpus to assign.
+    """
+
+    # Get the list of GPUs via nvidia-smi.
+    smi_query_result = subprocess.check_output(
+        "nvidia-smi -q -d Memory | grep -A4 GPU", shell=True
+    )
+    # Extract the usage information
+    gpu_info = smi_query_result.decode("utf-8").split("\n")
+    gpu_info = list(filter(lambda info: "Used" in info, gpu_info))
+    gpu_info = [
+        int(x.split(":")[1].replace("MiB", "").strip()) for x in gpu_info
+    ]
+
+    # Keep gpus under threshold only.
+    free_gpus = [
+        str(i) for i, mem in enumerate(gpu_info) if mem < threshold_vram_usage
+    ]
+    free_gpus = free_gpus[: min(max_gpus, len(free_gpus))]
+    gpus_to_use = ",".join(free_gpus)
+
+    if not gpus_to_use:
+        raise RuntimeError(tcols.FAIL + "No free GPUs found." + tcols.ENDC)
+
+    return gpus_to_use
 
 def import_hyperparams(hyperparams_file) -> dict:
     """
